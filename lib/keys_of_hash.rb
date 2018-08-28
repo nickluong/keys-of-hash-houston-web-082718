d28b821b5b7487e5f4b192af242bc4c20b446259
Nicholas  Luong <nckluong@gmail.com>
2018-08-28 14:16:50 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,4 +2,9 @@ class Hash
   def keys_of(hash, *arguments)
     # code goes here
     hash.collect do|word|
-      if(arguments.has_key)
+      if(arguments.has_key? word)
+        word
+      end
+    end
+  end
+end
